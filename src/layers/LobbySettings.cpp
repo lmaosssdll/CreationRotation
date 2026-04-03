5e3cc2947bbb988a6ee20cae723fa51a0e8dd7fe
lmaosssdll <h41015703@gmail.com>
2026-04-03 12:21:05 +0800

    My code is too bad

--- src/layers/LobbySettings.cpp
+++ src/layers/LobbySettings.cpp
@@ -5,7 +5,7 @@ enum LobbySettingType {
     Turns,
     MinsPerTurn,
     Password,
-    IsPublic 
+    IsPublic
 };
 
 class LobbySettingsCell : public CCNode {
@@ -13,19 +13,37 @@ protected:
     bool init(float width, std::string name, LobbySettingType type, std::string desc, std::string defaultStr = "", std::string filter = "") {
         this->type = type;
         
-        this->setContentSize({ width, CELL_HEIGHT });
+        this->setContentSize({
+            width, CELL_HEIGHT
+        });
 
-        auto nameLabel = CCLabelBMFont::create(name.c_str(), "bigFont.fnt");
-        nameLabel->setPosition({ 5.f, CELL_HEIGHT / 2.f });
-        nameLabel->setAnchorPoint({ 0.f, 0.5f });
+        auto nameLabel = CCLabelBMFont::create(
+            name.c_str(),
+            "bigFont.fnt"
+        );
+        
+        nameLabel->setPosition({
+            5.f, CELL_HEIGHT / 2.f
+        });
+        nameLabel->setAnchorPoint({
+            0.f, 0.5f
+        });
         nameLabel->limitLabelWidth(80.f, 0.5f, 0.1f);
         this->addChild(nameLabel);
 
-        auto infoBtn = InfoAlertButton::create(name, desc, 0.5f);
+        auto infoBtn = InfoAlertButton::create(
+            name,
+            desc,
+            0.5f
+        );
         auto menu = CCMenu::create();
         menu->addChild(infoBtn);
-        menu->setPosition({ nameLabel->getScaledContentWidth() + 15.f, CELL_HEIGHT / 2.f });
-        menu->setAnchorPoint({ 0.f, 0.5f });
+        menu->setPosition({
+            nameLabel->getScaledContentWidth() + 15.f, CELL_HEIGHT / 2.f
+        });
+        menu->setAnchorPoint({
+            0.f, 0.5f
+        });
         this->addChild(menu);
 
         switch (this->type) {
@@ -36,8 +54,12 @@ protected:
                 input = TextInput::create(95.f, name);
                 input->setString(defaultStr);
                 if (filter != "") input->getInputNode()->setAllowedChars(filter);
-                input->setPosition({ width - input->getContentWidth() - 5.f, CELL_HEIGHT / 2.f });
-                input->setAnchorPoint({ 0.f, 0.5f });
+                input->setPosition({
+                    width - input->getContentWidth() - 5.f, CELL_HEIGHT / 2.f
+                });
+                input->setAnchorPoint({
+                    0.f, 0.5f
+                });
                 this->addChild(input);
                 break;
             }
@@ -48,29 +70,18 @@ protected:
                         togglerVal = !toggler->isOn();
                     }
                 );
-                toggler->toggle(geode::utils::numFromString<int>(defaultStr).unwrapOr(0));
-                auto menu2 = CCMenu::create();
-                menu2->addChild(toggler);
-                menu2->setPosition({ width - toggler->getContentWidth() - 5.f, CELL_HEIGHT / 2.f });
-                menu2->setAnchorPoint({ 0.f, 0.5f });
-                this->addChild(menu2);
-                break;
-            }
-            case Tag: { // Логика для кнопки выбора тегов
-                tagIdx = geode::utils::numFromString<int>(defaultStr).unwrapOr(0);
-                if (tagIdx < 0 || tagIdx >= TAG_NAMES.size()) tagIdx = 0;
-
-                auto btnSpr = ButtonSprite::create(TAG_NAMES[tagIdx].c_str(), 80, true, "bigFont.fnt", "GJ_button_04.png", 30.f, 0.6f);
-                tagBtn = CCMenuItemExt::createSpriteExtra(btnSpr, [this](CCObject*) {
-                    tagIdx = (tagIdx + 1) % TAG_NAMES.size();
-                    auto newSpr = ButtonSprite::create(TAG_NAMES[tagIdx].c_str(), 80, true, "bigFont.fnt", "GJ_button_04.png", 30.f, 0.6f);
-                    tagBtn->setNormalImage(newSpr);
+                toggler->toggle(
+                    geode::utils::numFromString<int>(defaultStr).unwrapOr(0)
+                );
+                auto menu = CCMenu::create();
+                menu->addChild(toggler);
+                menu->setPosition({
+                    width - toggler->getContentWidth() - 5.f, CELL_HEIGHT / 2.f
                 });
-
-                auto menu3 = CCMenu::create();
-                menu3->addChild(tagBtn);
-                menu3->setPosition({ width - 50.f, CELL_HEIGHT / 2.f });
-                this->addChild(menu3);
+                menu->setAnchorPoint({
+                    0.f, 0.5f
+                });
+                this->addChild(menu);
                 break;
             }
         }
@@ -80,22 +91,38 @@ protected:
 public:
     static constexpr int CELL_HEIGHT = 35.f;
 
-    TextInput* input = nullptr;
-    CCMenuItemToggler* toggler = nullptr;
-    CCMenuItemSpriteExtra* tagBtn = nullptr;
+    TextInput* input;
+    CCMenuItemToggler* toggler;
 
     LobbySettingType type;
     bool togglerVal = false;
-    int tagIdx = 0; // Сохраняем индекс тега
 
     void save(LobbySettings& settings) {
         switch (this->type) {
-            case Name: settings.name = this->input->getString(); break;
-            case Turns: settings.turns = geode::utils::numFromString<int>(this->input->getString()).unwrapOr(0); break;
-            case MinsPerTurn: settings.minutesPerTurn = geode::utils::numFromString<int>(this->input->getString()).unwrapOr(1); break;
-            case Password: break;
-            case IsPublic: settings.isPublic = togglerVal; break;
-            case Tag: settings.tag = tagIdx; break; // Сохраняем выбранный тег
+            case Name: {
+                settings.name = this->input->getString();
+                break;
+            }
+            case Turns: {
+                settings.turns = geode::utils::numFromString<int>(
+                    this->input->getString()
+                ).unwrapOr(0);
+                break;
+            }
+            case MinsPerTurn: {
+                settings.minutesPerTurn = geode::utils::numFromString<int>(
+                    this->input->getString()
+                ).unwrapOr(1);
+                break;
+            }
+            case Password: {
+                // settings.password = this->input->getString();
+                break;
+            }
+            case IsPublic: {
+                settings.isPublic = togglerVal;
+                break;
+            }
         }
     }
 
@@ -121,36 +148,64 @@ LobbySettingsPopup* LobbySettingsPopup::create(LobbySettings const& settings, Ca
 }
 
 bool LobbySettingsPopup::init(LobbySettings const& settings, Callback callback) {
-    if (!Popup::init(250.f, 250.f)) { // Немного увеличил высоту для новой кнопки
+    if (!Popup::init(250.f, 230.f)) {
         return false;
     }
 
     m_noElasticity = true;
     this->setTitle("Lobby Settings");
 
+    CCNode* nameContainer = CCNode::create();
+    nameContainer->setLayout(
+        RowLayout::create()
+            ->setGap(-5.f)
+    );
+
     auto settingsContents = CCArray::create();
 
     #define ADD_SETTING(name, element, desc, type) settingsContents->addObject( \
-        LobbySettingsCell::create(220.f, name, LobbySettingType::type, desc, settings.element));
+        LobbySettingsCell::create( \
+            220.f, \
+            name,\
+            LobbySettingType::type, \
+            desc, \
+            settings.element \
+        ));
 
     #define ADD_SETTING_INT(name, element, desc, type) settingsContents->addObject( \
-        LobbySettingsCell::create(220.f, name, LobbySettingType::type, desc, std::to_string(settings.element), "0123456789"));
+        LobbySettingsCell::create( \
+            220.f, \
+            name,\
+            LobbySettingType::type, \
+            desc, \
+            std::to_string(settings.element), \
+            "0123456789" \
+        ));
 
     #define ADD_SETTING_BOOL(name, element, desc, type) settingsContents->addObject( \
-        LobbySettingsCell::create(220.f, name, LobbySettingType::type, desc, std::to_string(settings.element), ""));
+        LobbySettingsCell::create( \
+            220.f, \
+            name,\
+            LobbySettingType::type, \
+            desc, \
+            std::to_string(settings.element), \
+            "" \
+        ));
 
     ADD_SETTING("Name", name, "Name of the lobby", Name)
     ADD_SETTING_INT("Turns", turns, "Number of turns per level", Turns)
-    ADD_SETTING_INT("Minutes/Turn", minutesPerTurn, "Amount of minutes per turn", MinsPerTurn)
+    ADD_SETTING_INT("Minutes per turn", minutesPerTurn, "Amount of minutes per turn", MinsPerTurn)
     ADD_SETTING_BOOL("Public", isPublic, "Whether the swap is public. If it is, it will be shown in the room list for others to join.", IsPublic)
-    
-    // Добавляем тег в меню
-    ADD_SETTING_INT("Tag", tag, "Select a tag to describe your lobby", Tag)
+    // ADD_SETTING("Password", password, Password)
 
-    auto settingsList = ListView::create(settingsContents, LobbySettingsCell::CELL_HEIGHT, 220.f, 200.f);
+    auto settingsList = ListView::create(settingsContents, LobbySettingsCell::CELL_HEIGHT, 220.f, 180.f);
     settingsList->ignoreAnchorPointForPosition(false);
 
-    auto border = Border::create(settingsList, {0, 0, 0, 75}, {220.f, 200.f});
+    auto border = Border::create(
+        settingsList,
+        {0, 0, 0, 75},
+        {220.f, 180.f}
+    );
     if(auto borderSprite = typeinfo_cast<NineSlice*>(border->getChildByID("geode.loader/border_sprite"))) {
         float scaleFactor = 1.7f;
         borderSprite->setContentSize(CCSize{borderSprite->getContentSize().width, borderSprite->getContentSize().height + 3} / scaleFactor);
@@ -176,6 +231,7 @@ bool LobbySettingsPopup::init(LobbySettings const& settings, Callback callback) 
     );
     auto menu = CCMenu::create();
     menu->addChild(submitBtn);
+
     m_mainLayer->addChildAtPosition(menu, Anchor::Bottom);
 
     return true;
